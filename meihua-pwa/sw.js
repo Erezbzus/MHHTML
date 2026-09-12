d8e6a0784708ab3e64ef282e0a6261220193b0fd
Erezbzus <1171718263@qq.com>
2026-09-12 22:47:31 +0800

    Add files via upload

--- meihua-pwa/sw.js
+++ meihua-pwa/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "meihua-v1";
+const CACHE = "meihua-v3";
 const ASSETS = ["index.html", "manifest.webmanifest", "icon-192.png", "icon-512.png", "apple-touch-icon.png"];
 self.addEventListener("install", function (e) {
   e.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(ASSETS); }));
